b7a370cda2d2709bc72c5709924ad699fabfeb10
fawwazmayda <33770567+FawwazMayda@users.noreply.github.com>
2021-06-13 03:07:36 +0000

    exercise 11

--- RubyStudy-I/main.rb
+++ RubyStudy-I/main.rb
@@ -2,10 +2,10 @@ length = 9
 width = 8
 puts width
 puts length * width
-puts "----"
 
-# Perbarui variable width dengan 13
-width = 13
+puts "----"
+# Perbarui variable width dengan menambahkan 5 pada dirinya
+width += 5
 
 puts width
 puts length * width
