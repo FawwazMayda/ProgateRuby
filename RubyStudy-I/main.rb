cf3f045c597dd5b98ae1d582bd5735a5f7c09f32
fawwazmayda <33770567+FawwazMayda@users.noreply.github.com>
2021-06-13 03:12:33 +0000

    exercise 14

--- RubyStudy-I/main.rb
+++ RubyStudy-I/main.rb
@@ -1,6 +1,12 @@
-score = 92
+score = 80
 
-# Jika nilai score lebih besar daripada 80, cetak "Bagus sekali!"
-if score > 80
-  puts "Bagus sekali!"
+# Cetak hasil dari `score > 80`
+puts score > 80
+
+# Cetak hasil dari `score <= 80`
+puts score <= 80
+
+# Jika `score` kurang dari atau sama dengan 80, cetak "Anda bisa lebih baik lagi!"
+if score <= 80
+  puts "Anda bisa lebih baik lagi!"
 end
