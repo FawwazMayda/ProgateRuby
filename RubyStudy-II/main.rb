3e2f641a65a76d00a6499a656b83d0e907e4ab34
fawwazmayda <33770567+FawwazMayda@users.noreply.github.com>
2021-06-13 04:14:27 +0000

    exercise 7

--- RubyStudy-II/main.rb
+++ RubyStudy-II/main.rb
@@ -1,16 +1,5 @@
-exam = {"subject" => "Matematika", "score" => 80}
+# Tuis ulang hash berikut ini dengan menggunakan simbol-simbol
+exam = {:subject => "Matematika", :score => 80}
 
-# Cetak nilai dari element dengan kunci "subject"
-puts exam["subject"]
-
-# Perbarui nilai dari element dengan kunci "subject" menjadi "Ilmu Pengetahuan Alam"
-exam["subject"] = 'Ilmu Pengetahuan Alam'
-
-# Cetak lagi nilai dari element dengan kunci "subject"
-puts exam["subject"]
-
-# Tambahkan sebuah element dengan kunci "grade" dan nilai "B"
-exam["grade"] = "B"
-
-# Cetak nilai dari element dengan kunci "grade"
-puts exam["grade"]
+# Cetak nilai dari element dengan simbol :score
+puts exam[:score]
