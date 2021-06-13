b499bd5f37a3a817cccad422c5911cd60d3fac57
fawwazmayda <33770567+FawwazMayda@users.noreply.github.com>
2021-06-13 04:11:16 +0000

    exercise 6

--- RubyStudy-II/main.rb
+++ RubyStudy-II/main.rb
@@ -1,8 +1,16 @@
-# Tetapkan sebuah hash kedalam variable `exam`
-exam = {
-  "subject" => "Matematika",
-  "score" => 80
-}
+exam = {"subject" => "Matematika", "score" => 80}
 
-# Cetak variable `exam`
-puts exam
+# Cetak nilai dari element dengan kunci "subject"
+puts exam["subject"]
+
+# Perbarui nilai dari element dengan kunci "subject" menjadi "Ilmu Pengetahuan Alam"
+exam["subject"] = 'Ilmu Pengetahuan Alam'
+
+# Cetak lagi nilai dari element dengan kunci "subject"
+puts exam["subject"]
+
+# Tambahkan sebuah element dengan kunci "grade" dan nilai "B"
+exam["grade"] = "B"
+
+# Cetak nilai dari element dengan kunci "grade"
+puts exam["grade"]
