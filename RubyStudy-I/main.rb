3ba0538a52607a1d4cb55845d4836eaaa72f19dd
fawwazmayda <33770567+FawwazMayda@users.noreply.github.com>
2021-06-13 03:19:14 +0000

    exercise 17

--- RubyStudy-I/main.rb
+++ RubyStudy-I/main.rb
@@ -1,8 +1,10 @@
-score = 60
+score = 73
 
-# Tambahkan sebuah pernyataan `else` untuk mencetak "Anda bisa lebih baik lagi!"
+# Tambahkan sebuah pernyataan `elsif` untuk mencetak "Cukup bagus!"
 if score > 80
   puts "Bagus sekali!"
+elsif score > 60
+  puts "Cukup bagus!"
 else
   puts "Anda bisa lebih baik lagi!"
 end
